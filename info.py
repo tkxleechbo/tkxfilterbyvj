af0aaa62737e7d6dc8e5b3948d5d3ef8f70ba326
tkxleechbo <165795555+tkxleechbo@users.noreply.github.com>
2024-05-07 16:54:59 +0530

    Update info.py

--- info.py
+++ info.py
@@ -50,7 +50,7 @@ FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '')).
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://tkxleechbot:harishyu#81@@clustertkxvj.pb82jn2.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://tkxleechbot:harishyu81@clustertkxvj.ymwodbt.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "tkxmango")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'tkxdata')
 
