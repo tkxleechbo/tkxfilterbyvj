6fe7af4be44b985948746b2ee6f2b1817f9dd556
tkxleechbo <165795555+tkxleechbo@users.noreply.github.com>
2024-05-24 16:09:18 +0530

    Update info.py
    Update URL

--- info.py
+++ info.py
@@ -127,7 +127,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "https://tkxxmoviebot.onrender.com/")
+URL = environ.get("URL", "https://premiumfilter-c16u.onrender.com/")
 
 
 # Rename Info ; If True Then Bot Rename File Else Not
